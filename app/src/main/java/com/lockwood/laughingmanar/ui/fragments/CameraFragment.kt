ba2ead2bd0a45a7841d6ae060753f17a69748101
Ivan <izinovyev42@gmail.com>
2019-01-08 21:33:51 +0300

    remove Camera2

--- app/src/main/java/com/lockwood/laughingmanar/ui/fragments/CameraFragment.kt
+++ app/src/main/java/com/lockwood/laughingmanar/ui/fragments/CameraFragment.kt
@@ -2,46 +2,36 @@ package com.lockwood.laughingmanar.ui.fragments
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentActivity
 import android.support.v4.content.ContextCompat
-import android.support.v4.content.ContextCompat.checkSelfPermission
 import android.support.v4.view.GestureDetectorCompat
 import android.view.*
 import android.widget.ImageButton
 import com.lockwood.laughingmanar.R
-import com.lockwood.laughingmanar.REQUEST_CAMERA_PERMISSION
-import com.lockwood.laughingmanar.REQUEST_VIDEO_PERMISSIONS
-import com.lockwood.laughingmanar.VIDEO_PERMISSIONS
-import com.lockwood.laughingmanar.camera.CameraSource
 import com.lockwood.laughingmanar.extensions.ctx
 import com.lockwood.laughingmanar.extensions.openResFolder
 import com.lockwood.laughingmanar.ui.components.AutoFitTextureView
 import org.jetbrains.anko.alert
-import org.jetbrains.anko.cancelButton
 import org.jetbrains.anko.find
-import org.jetbrains.anko.okButton
 
 class CameraFragment : Fragment(), View.OnClickListener, View.OnTouchListener,
     ActivityCompat.OnRequestPermissionsResultCallback {
 
-    private lateinit var cameraSource: CameraSource
     private lateinit var textureView: AutoFitTextureView
     private lateinit var captureButton: ImageButton
 
     private val buttons = listOf(R.id.capture, R.id.info, R.id.swap)
     private val gestureDetector = GestureDetectorCompat(ctx, object : GestureDetector.SimpleOnGestureListener() {
         override fun onLongPress(e: MotionEvent) {
-            changCameraMode()
+//            changCameraMode()
         }
 
         override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
             val deltaY = Math.abs(e1.y - e2.y)
             if ((deltaY >= MIN_SWIPE_DISTANCE_Y) && (deltaY <= MAX_SWIPE_DISTANCE_Y)) {
-                cameraSource.swapCamera()
+//          TODO: swapCamera()
                 return true
             }
             return super.onFling(e1, e2, velocityX, velocityY)
@@ -69,40 +59,39 @@ class CameraFragment : Fragment(), View.OnClickListener, View.OnTouchListener,
         super.onActivityCreated(savedInstanceState)
         val permission = ContextCompat.checkSelfPermission(ctx, Manifest.permission.CAMERA)
         if (permission != PackageManager.PERMISSION_GRANTED) {
-            requestCameraPermission()
+//           requestCameraPermission()
             return
         }
-        cameraSource = CameraSource.getInstance(ctx, textureView)
     }
 
-    override fun onResume() = with(cameraSource) {
+    override fun onResume() {
         super.onResume()
-        update(ctx, textureView)
-        startBackgroundThread()
-        openCameraIfAvailable()
+//      TODO: openCameraIfAvailable()
     }
 
-    override fun onPause() = with(cameraSource) {
-        closeCamera()
-        stopBackgroundThread()
+    override fun onPause() {
+//      TODO: closeCamera()
         super.onPause()
     }
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.capture -> {
-                if (cameraSource.isVideoMode()) {
-                    handleVideo()
-                } else {
-                    cameraSource.lockFocus()
-                }
+                // TODO: implement capture
+//                if (isVideoMode()) {
+//                    handleVideo()
+//                } else {
+//                    lockFocus()
+//                }
             }
             R.id.info -> {
                 view.ctx.alert(R.string.intro_message) {
                     positiveButton("Results") { activity?.openResFolder() }
                 }.show()
             }
-            R.id.swap -> cameraSource.swapCamera()
+            R.id.swap -> {
+                // TODO: swapCamera()
+            }
         }
     }
 
@@ -112,82 +101,81 @@ class CameraFragment : Fragment(), View.OnClickListener, View.OnTouchListener,
     }
 
     fun onKeyDown(keyCode: Int, event: KeyEvent) {
-        cameraSource.lockFocus()
+//        TODO: lockFocus()
     }
 
-    private fun handleVideo() {
-        if (cameraSource.isRecordingVideo()) {
-            captureButton.setImageResource(R.drawable.ic_start)
-            cameraSource.stopRecordingVideo()
-        } else {
-            captureButton.setImageResource(R.drawable.ic_stop)
-            cameraSource.startRecordingVideo()
-        }
-    }
+//    private fun handleVideo() {
+//          if (isRecordingVideo()) {
+//            captureButton.setImageResource(R.drawable.ic_start)
+//              stopRecordingVideo()
+//        } else {
+//            captureButton.setImageResource(R.drawable.ic_stop)
+//             startRecordingVideo()
+//        }
+//    }
 
-    private fun changCameraMode() = with(cameraSource) {
-        changCameraMode()
-        if (isVideoMode()) {
-            captureButton.setImageResource(R.drawable.ic_start)
-        } else {
-            captureButton.setImageResource(R.drawable.ic_shutter)
-        }
-    }
+//    private fun changCameraMode()  {
+//        changCameraMode()
+//        if (isVideoMode()) {
+//            captureButton.setImageResource(R.drawable.ic_start)
+//        } else {
+//            captureButton.setImageResource(R.drawable.ic_shutter)
+//        }
+//    }
 
-    private fun shouldShowRequestPermissionRationale(permissions: Array<String>) =
-        permissions.any { shouldShowRequestPermissionRationale(it) }
-
-    private fun showPermissionAlert() {
-        ctx.alert(R.string.permission_request) {
-            okButton { parentFragment?.activity?.finish() }
-        }
-    }
-
-    private fun requestCameraPermission() {
-        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
-            showPermissionAlert()
-        } else {
-            requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
-        }
-    }
-
-    private fun hasPermissionsGranted(permissions: Array<String>) =
-        permissions.none {
-            checkSelfPermission((activity as FragmentActivity), it) != PERMISSION_GRANTED
-        }
-
-    private fun requestVideoPermissions() {
-        if (shouldShowRequestPermissionRationale(VIDEO_PERMISSIONS)) {
-            ctx.alert(R.string.permission_request) {
-                okButton { parentFragment?.requestPermissions(VIDEO_PERMISSIONS, REQUEST_VIDEO_PERMISSIONS) }
-                cancelButton { parentFragment?.activity?.finish() }
-            }
-        } else {
-            requestPermissions(VIDEO_PERMISSIONS, REQUEST_VIDEO_PERMISSIONS)
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_CAMERA_PERMISSION) {
-            if (grantResults.size != 1 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                showPermissionAlert()
-            }
-        } else if (requestCode == REQUEST_VIDEO_PERMISSIONS) {
-            if (grantResults.size == VIDEO_PERMISSIONS.size) {
-                for (result in grantResults) {
-                    if (result != PERMISSION_GRANTED) {
-                        showPermissionAlert()
-                        break
-                    }
-                }
-            } else {
-                showPermissionAlert()
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            cameraSource = CameraSource.getInstance(ctx, textureView)
-        }
-    }
+//    private fun shouldShowRequestPermissionRationale(permissions: Array<String>) =
+//        permissions.any { shouldShowRequestPermissionRationale(it) }
+//
+//    private fun showPermissionAlert() {
+//        ctx.alert(R.string.permission_request) {
+//            okButton { parentFragment?.activity?.finish() }
+//        }
+//    }
+//
+//    private fun requestCameraPermission() {
+//        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+//            showPermissionAlert()
+//        } else {
+//            requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+//        }
+//    }
+//
+//    private fun hasPermissionsGranted(permissions: Array<String>) =
+//        permissions.none {
+//            checkSelfPermission((activity as FragmentActivity), it) != PERMISSION_GRANTED
+//        }
+//
+//    private fun requestVideoPermissions() {
+//        if (shouldShowRequestPermissionRationale(VIDEO_PERMISSIONS)) {
+//            ctx.alert(R.string.permission_request) {
+//                okButton { parentFragment?.requestPermissions(VIDEO_PERMISSIONS, REQUEST_VIDEO_PERMISSIONS) }
+//                cancelButton { parentFragment?.activity?.finish() }
+//            }
+//        } else {
+//            requestPermissions(VIDEO_PERMISSIONS, REQUEST_VIDEO_PERMISSIONS)
+//        }
+//    }
+//
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+//        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+//            if (grantResults.size != 1 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+//                showPermissionAlert()
+//            }
+//        } else if (requestCode == REQUEST_VIDEO_PERMISSIONS) {
+//            if (grantResults.size == VIDEO_PERMISSIONS.size) {
+//                for (result in grantResults) {
+//                    if (result != PERMISSION_GRANTED) {
+//                        showPermissionAlert()
+//                        break
+//                    }
+//                }
+//            } else {
+//                showPermissionAlert()
+//            }
+//        } else {
+//            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        }
+//    }
 
     companion object {
         private const val MIN_SWIPE_DISTANCE_Y = 100
