fe66db0cb9bf31d092c83e335faa34ad856fc2ce
Ivan <izinovyev42@gmail.com>
2019-01-10 22:35:00 +0300

    update drawable

--- app/src/main/java/com/lockwood/laughingmanar/facedetection/FaceGraphic.kt
+++ app/src/main/java/com/lockwood/laughingmanar/facedetection/FaceGraphic.kt
@@ -2,6 +2,7 @@ package com.lockwood.laughingmanar.facedetection
 
 import android.graphics.*
 import android.graphics.Paint.Style
+import android.support.v4.content.ContextCompat
 import com.google.firebase.ml.vision.face.FirebaseVisionFace
 import com.lockwood.laughingmanar.R
 import com.lockwood.laughingmanar.extensions.centerX
@@ -16,6 +17,8 @@ class FaceGraphic(
     private val facing: Int
 ) : GraphicOverlay.Graphic(overlay) {
 
+    private val selectedImageType = ImageType.STATIC_SVG
+
     private val boxPaint = Paint().apply {
         color = Color.WHITE
         style = Style.STROKE
@@ -24,7 +27,7 @@ class FaceGraphic(
 
     override fun draw(canvas: Canvas) {
         val face = firebaseVisionFace ?: return
-//        drawFaceBorder(face, canvas)
+//        drawFaceBorder(face_svg, canvas)
         drawFaceImage(face, canvas)
     }
 
@@ -43,29 +46,60 @@ class FaceGraphic(
         canvas.drawRect(left, top, right, bottom, boxPaint)
     }
 
+    // TODO: add select model
     private fun drawFaceImage(
         face: FirebaseVisionFace,
         canvas: Canvas
     ) {
-        var faceBitmap: Bitmap = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.face)
-        val scaleFactory = FACE_SCALE_FACTOR
-        val xOffset = (face.width * scaleFactory).toInt()
-//        val yOffset = (face.height * xOffset / face.width * scaleFactory).toInt()
-        val yOffset = (face.height * scaleFactory).toInt()
+        when (selectedImageType) {
+            ImageType.STATIC_PNG -> {
+                // static image
+                var faceBitmap: Bitmap =
+                    BitmapFactory.decodeResource(applicationContext.resources, R.drawable.face_static)
+                val xOffset = (face.width * selectedImageType.scaleFactory).toInt()
+                val yOffset = (face.height * selectedImageType.scaleFactory).toInt()
+                // Scale the face
+                faceBitmap = Bitmap.createScaledBitmap(faceBitmap, xOffset, yOffset, false)
+                val x = translateX(face.centerX)
+                val y = translateY(face.centerY)
+                val left = x - xOffset / 2
+                val top = y - yOffset / 2
+                // Draw it
+                canvas.drawBitmap(faceBitmap, left, top, null)
+            }
+            ImageType.STATIC_SVG -> {
+                val drawable = ContextCompat.getDrawable(applicationContext, R.drawable.face_svg)
+                val x = translateX(face.centerX)
+                val y = translateY(face.centerY)
+                val xOffset = face.width * selectedImageType.scaleFactory
+                val yOffset = face.height * selectedImageType.scaleFactory
+                val left = (x - xOffset).toInt()
+                val top = (y - yOffset).toInt()
+                val right = (x + xOffset).toInt()
+                val bottom = (y + yOffset).toInt()
+                drawable?.let {
+                    it.setBounds(left, top, right, bottom)
+                    it.draw(canvas)
+                }
+            }
+            ImageType.ANIMATED_GIF -> {
 
-        // Scale the face
-        faceBitmap = Bitmap.createScaledBitmap(faceBitmap, xOffset, yOffset, false)
-        val x = translateX(face.centerX)
-        val y = translateY(face.centerY)
-        val left = x - xOffset / 2
-        val top = y - yOffset / 2
-        // Draw it
-        canvas.drawBitmap(faceBitmap, left, top, null)
+            }
+            ImageType.ANIMATED_SVG -> {
+
+            }
+        }
+    }
+
+    enum class ImageType(val scaleFactory: Float) {
+        STATIC_PNG(3.5f),
+        STATIC_SVG(2.0f),
+        ANIMATED_GIF(GRAPHIC_SCALE_FACTOR),
+        ANIMATED_SVG(GRAPHIC_SCALE_FACTOR)
     }
 
     companion object {
         private const val BOX_STROKE_WIDTH = 1.0f
         private const val GRAPHIC_SCALE_FACTOR = 1.5f
-        private const val FACE_SCALE_FACTOR = 3.5f
     }
 }
